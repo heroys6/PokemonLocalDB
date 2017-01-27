f078f1ddd5765f44981ff27a49b75c6ae6db25b2
heroys6 <0x000000xd@gmail.com>
2017-01-27 13:11:58 +0200

    Abstract class for working with different db. Added PostgreSQL support

--- src/main/java/com/home/db/PostgreSQL.java
+++ src/main/java/com/home/db/PostgreSQL.java
@@ -4,18 +4,27 @@ import com.home.pokemon.Pokemon;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import static com.home.db.Constants.*;
 
 /**
- * Created by User on 05.01.2017.
+ * Created by User on 25.01.2017.
  */
-public class MySQL extends DB {
+public class PostgreSQL extends DB {
 
-    public MySQL(String url, String uname, String pswd) {
+    private String uname, pswd, url;
+    Connection conn;
+
+    public PostgreSQL(String url, String uname, String pswd) {
+        this.uname = uname;
+        this.pswd = pswd;
+        this.url = url;
+
         try {
-            Connection conn = DriverManager.getConnection(url + dbUrlTail, uname, pswd);
+            // Connect to common db
+            conn = DriverManager.getConnection(url + dbName + dbUrlTail, uname, pswd);
 
             stm = conn.createStatement();
         } catch (SQLException e) {
@@ -26,17 +35,29 @@ public class MySQL extends DB {
     @Override
     public void createDB() {
         try {
+            // Close target db connection
+            stm.close();
+            conn.close();
+            // Create common db connection
+            conn = DriverManager.getConnection(url + dbUrlTail, uname, pswd);
+            stm = conn.createStatement();
+            // Trunk target db
             stm.executeUpdate("DROP DATABASE IF EXISTS " + dbName + ";");
             stm.executeUpdate("CREATE DATABASE " + dbName + ";");
+            // Reconnect to target db
+            stm.close();
+            conn.close();
+            conn = DriverManager.getConnection(url + dbName + dbUrlTail, uname, pswd);
+            stm = conn.createStatement();
             stm.executeUpdate(
-                    "CREATE TABLE " + dbName + "." + tableName + " (" +
-                            "id INT UNSIGNED NOT NULL AUTO_INCREMENT," +
+                    "CREATE TABLE " + tableName + " (" +
+                            "id SERIAL," +
                             "name VARCHAR(30) NOT NULL," +
-                            "attack INT UNSIGNED NOT NULL DEFAULT '0'," +
-                            "defense INT UNSIGNED NOT NULL DEFAULT '0'," +
-                            "stamina INT UNSIGNED NOT NULL DEFAULT '0'," +
-                            "cp_gain DECIMAL(5,2) UNSIGNED NOT NULL DEFAULT '0.0'," +
-                            "max_cp INT UNSIGNED NOT NULL DEFAULT '0'," +
+                            "attack INT NOT NULL DEFAULT 0," +
+                            "defense INT NOT NULL DEFAULT 0," +
+                            "stamina INT NOT NULL DEFAULT 0," +
+                            "cp_gain DECIMAL(5,2) NOT NULL DEFAULT 0.0," +
+                            "max_cp INT NOT NULL DEFAULT 0," +
                             "PRIMARY KEY(id)" +
                             ");"
             );
@@ -50,7 +71,7 @@ public class MySQL extends DB {
         ResultSet foo = null;
 
         try {
-            foo =  stm.executeQuery("SELECT * FROM " + dbName + "." + tableName + " WHERE name='" + pokName + "';");
+            foo =  stm.executeQuery("SELECT * FROM " + tableName + " WHERE name='" + pokName + "';");
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -61,7 +82,6 @@ public class MySQL extends DB {
     @Override
     public void addPokemon(Pokemon p) {
         try {
-            stm.executeQuery("USE " + dbName + ";");
             stm.executeUpdate(
                     "INSERT INTO " + tableName + " (name, attack, defense, stamina, cp_gain, max_cp) " +
                             "VALUES (" +
