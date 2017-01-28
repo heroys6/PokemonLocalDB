72be831e11a618c6e25585c1c209c12d6b2e4d1b
heroys6 <0x000000xd@gmail.com>
2017-01-28 09:44:42 +0200

    initial heroku commit

---
+++ src/main/java/com/home/server/Constants.java
@@ -0,0 +1,9 @@
+package com.home.server;
+
+/**
+ * Created by User on 26.01.2017.
+ */
+public abstract class Constants {
+    public final static String botName = "PokemonVS_bot";
+    public final static String botToken = "313114934:AAFF0XIgpiFlq8A7KH9Bgm8HaL6BeOSloDs";
+}
---
+++ src/main/java/com/home/server/TelegramBot.java
@@ -0,0 +1,56 @@
+package com.home.server;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import static com.home.server.Constants.*;
+
+/**
+ * Created by User on 27.01.2017.
+ */
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+
+        if (message.hasText()) {
+            SendMessage sendMes = new SendMessage();
+            sendMes.setChatId(message.getChatId().toString());
+            sendMes.setText("[test]|$echo: " + message.getText());
+            try {
+                sendMessage(sendMes);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    public static void main(String[] Args) {
+        ApiContextInitializer.init();
+
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        try {
+            botsApi.registerBot(new TelegramBot());
+            System.out.println("Runned...");
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
