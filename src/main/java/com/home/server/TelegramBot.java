db8067c35e4260d2d25e5bb7e287de9b0ddcc5f3
heroys6 <0x000000xd@gmail.com>
2017-01-28 21:41:49 +0200

    Font hotfix

--- src/main/java/com/home/server/TelegramBot.java
+++ src/main/java/com/home/server/TelegramBot.java
@@ -69,7 +69,7 @@ public class TelegramBot extends TelegramLongPollingBot {
 
                             // Stats
                             String stats = String.format(
-                                    "    att    def    stam    maxCP    gainCP\n%5s%5s%6s%7s%8.1f",
+                                    "     att     def     stam    maxCP    gainCP\n%5s%5s%6s%7s%8.1f",
                                     pokeInfo.getString("attack"),
                                     pokeInfo.getString("defense"),
                                     pokeInfo.getString("stamina"),
