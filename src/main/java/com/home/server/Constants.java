1c2b52c75eef2a5415e309752793db80d537f0c0
heroys6 <0x000000xd@gmail.com>
2017-01-31 15:33:15 +0200

    Bot name and token are hidden

--- src/main/java/com/home/server/Constants.java
+++ src/main/java/com/home/server/Constants.java
@@ -4,6 +4,6 @@ package com.home.server;
  * Created by User on 26.01.2017.
  */
 public abstract class Constants {
-    public final static String botName = "PokemonVS_bot";
-    public final static String botToken = "313114934:AAFF0XIgpiFlq8A7KH9Bgm8HaL6BeOSloDs";
+    public final static String botName = "your_bot_name";
+    public final static String botToken = "your_bot_token";
 }
